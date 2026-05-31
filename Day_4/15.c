6aa34f2227f8e212125b0450ec36dccfb4e5287b
ARCHIT SINGH <architsingh1422@gmail.com>
2026-05-31 06:04:21 +0530

    Create 15.c

---
+++ Day_4/15.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() 
+{
+    int num, originalNum, remainder, count = 0, result = 0;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    originalNum = num;
+    while (originalNum != 0) 
+    {
+        originalNum /= 10;
+        count++;
+    }
+
+    originalNum = num;
+
+    while (originalNum != 0) 
+    {
+        remainder = originalNum % 10;
+
+        result += round(pow(remainder, count)); 
+        
+        originalNum /= 10;
+    }
+
+    
+    if ((int)result == num) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+
+    return 0;
+}

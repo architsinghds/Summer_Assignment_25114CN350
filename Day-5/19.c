7f91813ca2c6692314f4d5d245152aaad9af0180
ARCHIT SINGH <architsingh1422@gmail.com>
2026-06-01 06:29:53 +0530

    Update 19.c

--- Day-5/19.c
+++ Day-5/19.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int num, i;
 
     printf("Enter a positive integer: ");
     scanf("%d", &num);
 
     printf("Prime Factors of %d are: ", num);
+    if (num%2==0)
+    {
+        printf ("2");
+        printf (" ");
+    }
 
-    for (i = 2; i <= num; ++i) {
+    for (i = 3; i <= num; i=i+2) 
+    {
         if (num % i == 0 && num!=i) 
         {
             printf("%d ", i);

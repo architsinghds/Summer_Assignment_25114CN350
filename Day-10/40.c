823b8fea5fb607eb2a3cf3b217687904c9e1d2e7
ARCHIT SINGH <architsingh1422@gmail.com>
2026-06-10 08:10:05 +0530

    Create 40.c

---
+++ Day-10/40.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (int i = 1; i <= rows; i++) 
+    {
+        for (int j = i; j < rows; j++) 
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) 
+        {
+            printf("%c", 'A' + (i - 1));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

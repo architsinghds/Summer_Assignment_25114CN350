434ae6d4875c54ffdc2023042176326a1fb288c0
ARCHIT SINGH <architsingh1422@gmail.com>
2026-06-12 14:27:25 +0530

    Create 47.c

---
+++ Day-12/47.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+void printFibonacciIterative(int n) 
+{
+    int t1 = 0, t2 = 1, nextTerm;
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 1; i <= n; ++i) 
+    {
+        printf("%d ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    printf("\n");
+}
+
+int main() 
+{
+    int terms;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &terms);
+
+    if (terms <= 0) 
+    {
+        printf("Please enter a positive integer.\n");
+    } 
+    else 
+    {
+        printFibonacciIterative(terms);
+    }
+
+    return 0;
+}

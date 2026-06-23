57e24d92a4efee57a7d00eae3826a5c9d8433f63
ARCHIT SINGH <architsingh1422@gmail.com>
2026-06-23 19:17:52 +0530

    Create 89.c

---
+++ Day-23/89.c
@@ -0,0 +1,21 @@
+#include<stdio.h> 
+int main()
+{
+    char str[100]="Entered string";
+    int i;
+    int freq[256] = {0};
+    //Calculating frequency of each character.
+    for(i = 0; str[i] != '\0'; i++)
+    {
+        freq[str[i]]++;
+    }
+ printf("The non repeating characters are: ");
+  for(i = 0; i < 256; i++)
+  {
+    if(freq[i] == 1)
+     {
+      printf(" %c ", i);
+     }
+  }
+  return 0;
+ }

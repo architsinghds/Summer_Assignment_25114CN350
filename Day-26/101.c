704df53a518d1c1c4e02071075c03e4ad43ce8b0
ARCHIT SINGH <architsingh1422@gmail.com>
2026-06-26 21:36:43 +0530

    Create 101.c

---
+++ Day-26/101.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main() 
+{
+    int secret_number, guess;
+    int attempts = 0;
+    srand(time(NULL));
+    secret_number = (rand() % 100) + 1;
+
+    printf("=========================================\n");
+    printf("    Welcome to the Number Guessing Game! \n");
+    printf("=========================================\n");
+    printf("I have chosen a number between 1 and 100.\n");
+    printf("Can you guess what it is?\n\n");
+    do 
+    {
+        printf("Enter your guess: ");
+        if (scanf("%d", &guess) != 1) 
+        {
+            printf("Invalid input! Please enter a valid integer.\n\n");
+            while (getchar() != '\n'); 
+            continue;
+        }
+        attempts++;
+        if (guess > secret_number) 
+        {
+            printf("Too high! Try a smaller number.\n\n");
+        } 
+        else if (guess < secret_number) 
+        {
+            printf("Too low! Try a larger number.\n\n");
+        } 
+        else 
+        {
+            printf("\n🎉 Congratulations! You guessed it right!\n");
+            printf("The secret number was %d.\n", secret_number);
+            printf("It took you %d attempts to win.\n", attempts);
+        }
+    } 
+    while (guess != secret_number);
+    return 0;
+}
